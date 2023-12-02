6cf1a7e7df9fc0b38c08ef3ce01081b16881497f
Kauê Bertaze <69527468+KaueTTS@users.noreply.github.com>
2023-12-02 20:40:18 -0300

    #

---
+++ Fundamentos JS/funcoes.js
@@ -0,0 +1,19 @@
+// let x = ''
+// console.log(x)
+// x = 'oi'
+
+// Três formas de escrever funções
+
+// Declaração de função
+function imprimeTexto(texto) {
+    console.log(texto)
+}
+
+imprimeTexto(soma())
+// imprimeTexto('Outro texto')
+
+function soma() {
+    return 2 + 2
+}
+
+// console.log(soma())
