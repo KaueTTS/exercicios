2805c10cb6cb5616a3c237ce050e79ddd32c4407
Kauê Bertaze <69527468+KaueTTS@users.noreply.github.com>
2023-12-10 19:18:42 -0300

    #

---
+++ Fundamentos JS/for.js
@@ -0,0 +1,8 @@
+const nmrs = [100, 200, 300, 400, 500]
+
+// Primeira expressão: é executada apenas uma única vez
+// Segunda expressão: condição de execução
+// Terceira expressão: é executado sempre ao final do bloco
+for (let i = 0; i < nmrs.length; i++) {
+    console.log(nmrs[i])
+}
