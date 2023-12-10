e95361a50b44f7ff51655911868569cc63d9a896
Kauê Bertaze <69527468+KaueTTS@users.noreply.github.com>
2023-12-10 10:24:45 -0300

    #

---
+++ Fundamentos JS/array-push.js
@@ -0,0 +1,10 @@
+const notas = [10, 6, 8]
+
+// Acrescenta um valor no array [10, 6, 8 + 7]
+notas.push(7)
+
+console.log(notas)
+
+const media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length
+
+console.log(media)
