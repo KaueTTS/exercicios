1b320df97cbbc163ea0270acfbc5624b930dc8e0
Kauê Bertaze <69527468+KaueTTS@users.noreply.github.com>
2023-12-10 16:51:51 -0300

    #

---
+++ Fundamentos JS/array-concat.js
@@ -0,0 +1,6 @@
+const salaJS = ['Kauê', 'Guilherme', 'Leonardo', 'Mariana']
+const salaPY = ['Amanda', 'Camila', 'Pablo', 'Júlia']
+
+const salas = salaJS.concat(salaPY)
+
+console.log(salas)
---
+++ Fundamentos JS/array-dimensoes.js
@@ -0,0 +1,9 @@
+const alunos = ['João', 'Juliana', 'Ana', 'Caio']
+const medias = [10, 8, 7.5, 9]
+
+const listaAlunosEMedias = [alunos, medias]
+
+console.log(`
+    A aluna da posição 1 da lista de alunos é: ${listaAlunosEMedias[0][1]}.
+    A média dessa aluna é ${listaAlunosEMedias[1][1]}.    
+`)
