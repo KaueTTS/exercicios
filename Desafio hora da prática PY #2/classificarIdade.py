881025fc2f32ba5af0b17b3217e9df56656921b3
Kauê Bertaze <69527468+KaueTTS@users.noreply.github.com>
2023-12-17 06:22:14 -0300

    Avançando nos estudos com Python

---
+++ Desafio hora da prática PY #2/classificarIdade.py
@@ -0,0 +1,8 @@
+idade = int(input('Digite sua idade: '))
+
+if idade <= 12:
+    print('Você é uma criança!')
+elif 12 < idade < 18:
+    print('Você é um adolescente!')
+else:
+    print('Você é um adulto!')
