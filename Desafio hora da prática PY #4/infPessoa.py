fdf0eff7ace651aa332051a3550d59f018385f22
Kauê Bertaze <69527468+KaueTTS@users.noreply.github.com>
2023-12-17 20:17:19 -0300

    Avançando nos estudos com python

---
+++ Desafio hora da prática PY #4/infPessoa.py
@@ -0,0 +1,16 @@
+pessoa = {
+    'nome': 'Kauê',
+    'idade': '19',
+    'cidade': 'Diadema',
+    'genero': 'Masculino',
+    'profissao': 'Suporte Técnico'
+}
+
+# Atualização de Idade
+pessoa['idade'] = 20
+
+# Adicionando Profissão
+pessoa['cidade'] = 'São paulo'
+
+# Remoção de Elemento
+del pessoa['profissao']
---
+++ Desafio hora da prática PY #4/verificarChave.py
@@ -0,0 +1,8 @@
+pessoa = {'nome': 'Amanda', 
+          'idade': 19, 
+          'cidade': 'São Paulo'}
+
+if 'nome' in pessoa:
+    print("A chave 'nome' existe no dicionário.")
+else:
+    print("A chave 'nome' não existe no dicionário.")
