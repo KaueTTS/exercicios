619109b82c8a820ace9def84d7a3a8232b75bb39
Kauê Bertaze <69527468+KaueTTS@users.noreply.github.com>
2023-12-30 13:53:39 -0300

    Avançando nos estudos com JS

--- Fundamentos JS/map-string.js
+++ Fundamentos JS/map-string.js
@@ -1,7 +1,9 @@
 const nomes = ['ana Julia', 'Caio vinicius', 'BIA silva']
 
-const nomesPadronizados = nomes.map((nome) => {
-    return nome.toLowerCase() // ou toUpperCase()
-})
+// const nomesPadronizados = nomes.map((nome) => {
+//     return nome.toLowerCase() // ou toUpperCase()
+// })
+
+const nomesPadronizados = nomes.map((nome) => nome.toLowerCase())
 
 console.log(nomesPadronizados)
