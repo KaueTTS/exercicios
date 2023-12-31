d704cb90a1d4898d44d7b25920673eaad3ce2e44
Kauê Bertaze <69527468+KaueTTS@users.noreply.github.com>
2023-12-31 16:03:14 -0300

    Avançando nos estudos com JS

--- Fundamentos JS/objeto.js
+++ Fundamentos JS/objeto.js
@@ -7,3 +7,9 @@ const cliente = {
 
 console.log(`O nome do cliente é ${cliente['nome']} e essa pessoa tem ${cliente.idade} anos.`)
 console.log(`O seu CPF é o ${cliente.CPF.substring(0, 2)}x.xxx.xxx-${cliente.CPF.substring(9, 11)}`)
+
+const chaves = ['nome', 'idade', 'CPF', 'email', 'altura']
+
+chaves.forEach((chave) => {
+    console.log(`A chave ${chave} tem valor de ${cliente[chave]}`)
+})
