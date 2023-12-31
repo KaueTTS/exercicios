d6d79081ae232b307a139959131ab460866b6759
Kauê Bertaze <69527468+KaueTTS@users.noreply.github.com>
2023-12-31 15:59:03 -0300

    Avançando nos estudos com JS

--- Fundamentos JS/objeto.js
+++ Fundamentos JS/objeto.js
@@ -1,7 +1,9 @@
 const cliente = {
     nome: 'Kauê Bertaze de Oliveira',
     idade: 19,
-    CPF: '88899922255',
+    CPF: '44899922255',
     email: 'kaue@gmail.com.br'
 }
 
+console.log(`O nome do cliente é ${cliente['nome']} e essa pessoa tem ${cliente.idade} anos.`)
+console.log(`O seu CPF é o ${cliente.CPF.substring(0, 2)}x.xxx.xxx-${cliente.CPF.substring(9, 11)}`)
