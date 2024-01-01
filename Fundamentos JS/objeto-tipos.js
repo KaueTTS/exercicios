1b6bc296e26a69ea943070f872c781a56b8d9d18
Kauê Bertaze <69527468+KaueTTS@users.noreply.github.com>
2024-01-01 09:57:36 -0300

    Avançando nos estudos com JS

--- Fundamentos JS/objeto-tipos.js
+++ Fundamentos JS/objeto-tipos.js
@@ -7,3 +7,24 @@ const cliente = {
         '11 988899822'
     ]
 }
+
+cliente.enderecos = [
+    {
+        rua: 'R. Joseph Climber',
+        numero: 1337,
+        apartamento: true,
+        complemento: 'ap 934'
+    },
+]
+
+cliente.enderecos.push({
+    rua: 'R. Joseph Ladder',
+    numero: 404,
+    apartamento: false
+})
+
+const listaApenasApartamentos = cliente.enderecos.filter(
+    (endereco) => endereco.apartamento === true
+)
+
+console.log(listaApenasApartamentos)
