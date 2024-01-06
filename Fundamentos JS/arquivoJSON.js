9639110779cf924a807efb98328c27bfc6b78c95
Kauê Bertaze <69527468+KaueTTS@users.noreply.github.com>
2024-01-06 12:17:08 -0300

    Avançando nos estudos com JS

--- Fundamentos JS/arquivoJSON.js
+++ Fundamentos JS/arquivoJSON.js
@@ -1,5 +1,16 @@
 const dados = require('./arquivo.json')
 
 console.log(dados)
-console.log()
 console.log(typeof dados)
+
+const clienteEmString = JSON.stringify(dados)
+
+console.log()
+console.log(clienteEmString)
+console.log(typeof clienteEmString)
+
+const objCliente = JSON.parse(clienteEmString)
+
+console.log()
+console.log(objCliente)
+console.log(typeof objCliente)
